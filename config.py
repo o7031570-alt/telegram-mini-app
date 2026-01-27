a0cfbc860cc8a1799e223feeb1116beeb4091ce5
o7031570-alt <o7031570@gmail.com>
2026-01-28 06:46:01 +0700

    New Update

---
+++ config.py
@@ -0,0 +1,13 @@
+# Telegram Bot Configuration
+BOT_TOKEN = "8514370308:AAG-qf5sR3IV9Ad0T0RZM9xCXv-59FPyR7I"
+CHANNEL_USERNAME = "@for_you_today"
+CHANNEL_ID = -1003791270028
+ADMIN_USER_ID = 7252765971
+
+# Initially use your GitHub Pages URL, will be updated by Ngrok
+MINI_APP_URL = "https://o7031570-alt.github.io/telegram-mini-app/"
+
+# Backend Configuration
+FLASK_HOST = "127.0.0.1"
+FLASK_PORT = 5000
+DATABASE_PATH = "database/posts.db"
